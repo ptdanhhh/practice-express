e8515078d34ffe19ce475b56817f106ac12d4d97
Danh Pham <danhpham612@gmail.com>
2025-01-19 11:56:48 -0800

    Hexagonal Practice

--- hexagonal/src/index.ts
+++ hexagonal/src/index.ts
@@ -1,14 +1,16 @@
 import express, { Request, Response } from "express";
-import { setupTopicHTTPModule } from "./topic/module";
 import morgan from "morgan";
+import dotenv from "dotenv";
+import { setupTopicHTTPModule } from "./topic/module";
 
+dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
 //We have to tell express to use json
 //so express can understand and return correct data
 app.use(express.json());
-app.use(morgan("combined"));
+app.use(morgan("combined")); //Http logging
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
